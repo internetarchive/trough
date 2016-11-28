35b7ad40f022ef336d79742d4f03dd70cbe63b64
James Kafader <jkafader@archive.org>
2016-11-28 15:47:21 -0800

    added streaming support

--- bin/read.py
+++ bin/read.py
@@ -1,43 +1,23 @@
 #!/usr/bin/env python3
-# TODO: we also need to solve streaming. Maybe ijson?
-#from application import app
 from bin.settings import settings
 import sqlite3
 import ujson
 import os
 import sqlparse
 import logging
-#from mixins import ThreadPoolMixIn
-#from http.server import HTTPServer, BaseHTTPRequestHandler
 
-# class ReadHandler(BaseHTTPRequestHandler):
-#     def do_POST(self):
-#         database = os.path.split(self.path)[-1]
-#         database = os.path.join(settings['DATA_ROOT'], "{name}.sqlite".format(name=database))
-#         connection = sqlite3.connect(database)
-#         cur = connection.cursor()
-
-#         query = self.rfile.readline().strip()
-#         logging.info('Servicing request: {query}'.format(query=query))
-#         if len(sqlparse.split(query)) != 1:
-#             raise Exception('Exactly one SELECT query at a time.')
-#         parsed = sqlparse.parse(query)
-
-#         cur.execute(query.decode('utf-8'))
-#         output = [dict((cur.description[i][0], value) \
-#             for i, value in enumerate(row)) for row in cur.fetchall()]
-#         cur.close()
-#         connection.close()
-
-#         return self.wfile.write(ujson.dumps(output).encode('utf-8'))
-
-#class ThreadedHTTPServer(ThreadPoolMixIn, HTTPServer):
-#    """Handle requests in a separate thread."""
-#    numThreads = int(settings.get('READ_THREADS', 8))
-
-#server = ThreadedHTTPServer(('0.0.0.0', 8080), ReadHandler)
-#logging.warn('Starting server with {threads} threads, use <Ctrl-C> to stop'.format(threads=server.numThreads))
-#server.serve_forever()
+def stream_output(cursor, query):
+    yield b"["
+    first = True
+    for row in cursor.execute(query.decode('utf-8')):
+        if not first:
+            yield b","
+        output = dict((cursor.description[i][0], value) for i, value in enumerate(row))
+        yield ujson.dumps(output).encode('utf-8')
+        first = False
+    yield b"]"
+    cursor.close()
+    cursor.connection.close()
 
 def read(env, start_response):
     try:
@@ -52,14 +32,8 @@ def read(env, start_response):
             raise Exception('Exactly one SELECT query at a time.')
         parsed = sqlparse.parse(query)
 
-        cur.execute(query.decode('utf-8'))
-        output = [dict((cur.description[i][0], value) \
-            for i, value in enumerate(row)) for row in cur.fetchall()]
-
-        cur.close()
-        connection.close()
         start_response('200 OK', [('Content-Type','application/json')])
-        return [ujson.dumps(output).encode('utf-8')]
+        return stream_output(cur, query)
 
     except Exception as e:
         start_response('500 Server Error', [('Content-Type', 'text/plain')])
