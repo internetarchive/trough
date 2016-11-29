712b56591f8f969ff1c7a9c7fc0288f62263f592
James Kafader <jkafader@archive.org>
2016-11-29 11:05:39 -0800

    added SELECT-only validation, removed obsolete thread-pooling mixin in favor of uwsgi.

--- bin/read.py
+++ bin/read.py
@@ -6,16 +6,22 @@ import os
 import sqlparse
 import logging
 
-def stream_output(cursor, query):
-    yield b"["
+def stream_output(cursor, query, start_response):
     first = True
-    for row in cursor.execute(query.decode('utf-8')):
-        if not first:
-            yield b","
-        output = dict((cursor.description[i][0], value) for i, value in enumerate(row))
-        yield ujson.dumps(output).encode('utf-8')
-        first = False
-    yield b"]"
+    try:
+        for row in cursor.execute(query.decode('utf-8')):
+            if first:
+                start_response('200 OK', [('Content-Type','application/json')])
+                yield b"["
+            if not first:
+                yield b","
+            output = dict((cursor.description[i][0], value) for i, value in enumerate(row))
+            yield ujson.dumps(output).encode('utf-8')
+            first = False
+        yield b"]"
+    except Exception as e:
+        start_response('500 Server Error', [('Content-Type', 'text/plain')])
+        yield [b'500 Server Error: %s' % str(e).encode('utf-8')]
     cursor.close()
     cursor.connection.close()
 
@@ -28,12 +34,10 @@ def read(env, start_response):
 
         query = env.get('wsgi.input').read()
         logging.info('Servicing request: {query}'.format(query=query))
-        if len(sqlparse.split(query)) != 1:
-            raise Exception('Exactly one SELECT query at a time.')
-        parsed = sqlparse.parse(query)
-
-        start_response('200 OK', [('Content-Type','application/json')])
-        return stream_output(cur, query)
+        # if the user sent more than one query, or the query is not a SELECT, raise an exception.
+        if len(sqlparse.split(query)) != 1 or sqlparse.parse(query)[0].get_type() != 'SELECT':
+            raise Exception('Exactly one SELECT query per request, please.')
+        return stream_output(cur, query, start_response)
 
     except Exception as e:
         start_response('500 Server Error', [('Content-Type', 'text/plain')])
