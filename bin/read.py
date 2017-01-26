2f0ec712bd61ebd01ae27e6a7086e02d88cd8c2c
James Kafader <jkafader@archive.org>
2017-01-26 08:13:01 -0800

    refactored 'read' module to use a class, for unit tests.

--- bin/read.py
+++ bin/read.py
@@ -6,39 +6,45 @@ import os
 import sqlparse
 import logging
 
-def stream_output(cursor, query, start_response):
-    first = True
-    try:
-        for row in cursor.execute(query.decode('utf-8')):
-            if first:
-                start_response('200 OK', [('Content-Type','application/json')])
-                yield b"["
-            if not first:
-                yield b","
-            output = dict((cursor.description[i][0], value) for i, value in enumerate(row))
-            yield ujson.dumps(output).encode('utf-8')
-            first = False
-        yield b"]"
-    except Exception as e:
-        start_response('500 Server Error', [('Content-Type', 'text/plain')])
-        yield [b'500 Server Error: %s' % str(e).encode('utf-8')]
-    cursor.close()
-    cursor.connection.close()
+# TODO: Refactor to class 'ReadServer'
 
-def application(env, start_response):
-    try:
-        database = env.get('HTTP_HOST', "").split(".")[0] # get database id from host/request path
-        database = os.path.join(settings['LOCAL_DATA'], "{name}.sqlite".format(name=database))
-        connection = sqlite3.connect(database)
-        cur = connection.cursor()
+class ReadServer:
+    def __init__(segment_name):
+        self.segment_name = segment_name
+        self.segment_path = os.path.join(settings['LOCAL_DATA'], "{name}.sqlite".format(name=database))
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
 
-        query = env.get('wsgi.input').read()
+    def stream_output(self, start_response):
+        first = True
+        try:
+            for row in self.cursor.execute(query.decode('utf-8')):
+                if first:
+                    start_response('200 OK', [('Content-Type','application/json')])
+                    yield b"["
+                if not first:
+                    yield b","
+                output = dict((self.cursor.description[i][0], value) for i, value in enumerate(row))
+                yield ujson.dumps(output).encode('utf-8')
+                first = False
+            yield b"]"
+        except Exception as e:
+            start_response('500 Server Error', [('Content-Type', 'text/plain')])
+            yield [b'500 Server Error: %s' % str(e).encode('utf-8')]
+        self.cursor.close()
+        self.cursor.connection.close()
+
+    def read(query, start_response)
         logging.info('Servicing request: {query}'.format(query=query))
         # if the user sent more than one query, or the query is not a SELECT, raise an exception.
         if len(sqlparse.split(query)) != 1 or sqlparse.parse(query)[0].get_type() != 'SELECT':
             raise Exception('Exactly one SELECT query per request, please.')
-        return stream_output(cur, query, start_response)
+        return self.stream_output(start_response)
 
+def application(env):
+    try:
+        segment_name = env.get('HTTP_HOST', "").split(".")[0] # get database id from host/request path
+        return ReadServer(segment_name, start_response).read(env.get('wsgi.input').read(), start_response)
     except Exception as e:
         start_response('500 Server Error', [('Content-Type', 'text/plain')])
         return [b'500 Server Error: %s' % str(e).encode('utf-8')]
