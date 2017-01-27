bc85321edb046d6db5440d7d39ef7de07d248b10
James Kafader <jkafader@archive.org>
2017-01-26 16:00:09 -0800

    changes to write process after building tests

--- trough/write.py
+++ trough/write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from bin.settings import settings
+from trough.settings import settings
 import sqlite3
 import ujson
 import os
@@ -7,27 +7,34 @@ import sqlparse
 import logging
 
 class WriteServer:
-    def __init__(segment_name):
-        self.segment_name = segment_name
-        self.segment_path = os.path.join(settings['LOCAL_DATA'], "{name}.sqlite".format(name=database))
-        self.connection = sqlite3.connect(database)
-        self.cursor = self.connection.cursor()
+    def __init__(self, start_response):
+        self.start_response = start_response
 
-    def write(self, query, start_response):
+    def write(self, segment_name, query):
         logging.info('Servicing request: {query}'.format(query=query))
-        # if the user sent more than one query, or the query is not a SELECT, raise an exception.
-        for q in sqlparse.parse(q).get_type() not in ['INSERT', 'UPDATE', 'DELETE', 'CREATE', 'ALTER']:
-            raise Exception('This server only accepts "Write" queries, that is "INSERT", "UPDATE", "DELETE", "CREATE" or "ALTER".')
-        output = self.cursor.execute(query.decode('utf-8'))
-        self.cursor.close()
-        self.cursor.connection.close()
-        return str(output).encode('utf-8')
+        # if one or more of the query(s) are not a write query, raise an exception.
+        if not query:
+            raise Exception("No query provided.")
+        for q in sqlparse.parse(query):
+            if q.get_type() not in ['INSERT', 'UPDATE', 'DELETE', 'CREATE', 'ALTER']:
+                raise Exception('This server only accepts "Write" queries, that is, queries that begin with "INSERT", "UPDATE", "DELETE", "CREATE" or "ALTER".')
+        connection = sqlite3.connect(segment_name)
+        cursor = connection.cursor()
+        try:
+            output = cursor.execute(query.decode('utf-8'))
+        finally:
+            cursor.close()
+            connection.commit()
+            connection.close()
+        return b"OK"
 
-def application(env):
+def application(env, start_response):
     try:
         segment_name = env.get('HTTP_HOST', "").split(".")[0] # get database id from host/request path
         query = env.get('wsgi.input').read()
-        return WriteServer(segment_name).write(query, start_response)
+        self.segment_name = segment_name
+        self.segment_path = os.path.join(settings['LOCAL_DATA'], "{name}.sqlite".format(name=segment_name))
+        return WriteServer(start_response).write(segment_name, query)
     except Exception as e:
         start_response('500 Server Error', [('Content-Type', 'text/plain')])
         return [b'500 Server Error: %s' % str(e).encode('utf-8')]
