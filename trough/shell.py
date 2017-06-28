abd17e5c702fa2c63ebd68ba7a768ebc70c97a84
James Kafader <jkafader@archive.org>
2017-06-27 23:15:28 -0700

    added some more useful help

--- trough/shell.py
+++ trough/shell.py
@@ -12,7 +12,13 @@ class TroughShell(cmd.Cmd):
     _connection = None
 
     def do_query(self, query):
-        'Send a query to the currently-connected trough segment'
+        '''Send a query to the currently-connected trough segment.
+
+        Syntax: query sql_query
+
+        Example: Send query "select * from host_statistics;" to server
+        trough> query select * from host_statistics;
+        '''
         cursor = self._connection.cursor()
         start = datetime.datetime.now()
         cursor.execute(query)
@@ -30,7 +36,13 @@ class TroughShell(cmd.Cmd):
         print("%s results in %s" % (len(output), end - start))
 
     def do_connect(self, args):
-        'Connect to a trough segment (remote sqlite database)'
+        '''Connect to a trough segment (remote sqlite database).
+
+        Syntax: connect database rethinkdb_host [proxy] [proxy_port] [proxy_type]
+
+        Example: Connect to database 190123 with rethinkdb running on wbgrp-svc100 and a SOCKS5 proxy running on localhost:9000:
+        trough> connect 190123 wbgrp-svc100 localhost 9000
+        '''
         args = args.split(" ")
         database = args[0]
         rethinkdb = args[1]
@@ -48,12 +60,25 @@ class TroughShell(cmd.Cmd):
         print('connected to %s' % database)
 
     def do_bye(self, arg):
-        'close connection and exit: BYE'
+        '''Close connection and exit.
+
+        Syntax: bye
+
+        Example:
+        trough> bye
+        Thank you for using trough.'''
         print('\nThank you for using trough.')
         self.close()
         return True
 
     def do_EOF(self, arg):
+        '''Close connection and exit.
+
+        Syntax: ^D
+
+        Example:
+        trough> ^D
+        Thank you for using trough.'''
         return self.do_bye(arg)
 
     def close(self):
