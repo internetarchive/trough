869a5d6f17e44ed856a735996f286204799b4eba
Noah Levitt <nlevitt@archive.org>
2017-10-19 14:20:20 -0700

    make run_tests.sh work

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,24 +8,25 @@ docker build -t internetarchive/trough-tests $script_dir
 
 # TODO some of these uwsgi services are not what we run anymore
 
-for python in python3
-do
-    docker run --rm -it --volume="$script_dir/..:/trough" internetarchive/rethinkdb-plus-hdfs /sbin/my_init -- \
-        bash -x -c "cd /tmp && git clone /trough \
-                && cd /tmp/trough \
-                && (cd /trough && git diff HEAD) | patch -p1 \
-                && virtualenv -p $python /tmp/venv \
-                && source /tmp/venv/bin/activate \
-                && pip install -e /trough --no-input --upgrade --pre --index-url https://devpi.archive.org/ait/packages/+simple/ \
-                && sync.py >>/tmp/trough-sync-local.out 2>&1
-                & sleep 5
-                && sync.py --server >>/tmp/trough-sync-server.out 2>&1
-                & uwsgi --venv=/tmp/venv --http :6444 --master --processes=2 --harakiri=3200 --socket-timeout=3200 --max-requests=50000 --vacuum --die-on-term --wsgi-file /tmp/venv/bin/reader.py >>/tmp/trough-read.out 2>&1
-                & uwsgi --venv=/tmp/venv --http :6222 --master --processes=2 --harakiri=240 --max-requests=50000 --vacuum --die-on-term --wsgi-file /tmp/venv/bin/writer.py >>/tmp/trough-write.out 2>&1
-                & uwsgi --venv=/tmp/venv --http :6112 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --wsgi-file /tmp/venv/bin/write_provisioner_local.py >>/tmp/trough-write-provisioner-local.out 2>&1
-                & uwsgi --venv=/tmp/venv --http :6111 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --wsgi-file /tmp/venv/bin/write_provisioner_server.py >>/tmp/trough-write-provisioner-server.out
-                && pip install pytest \
-                && py.test -v tests"
-done
-
-
+docker run --rm -it --volume="$script_dir/..:/trough" internetarchive/trough-tests /sbin/my_init -- bash -c \
+    $'bash -x -c "cd /tmp && git clone /trough \
+            && cd /tmp/trough \
+            && (cd /trough && git diff HEAD) | patch -p1 \
+            && virtualenv -p python3 /tmp/venv \
+            && source /tmp/venv/bin/activate \
+            && pip install -e /trough --no-input --upgrade --pre --index-url https://devpi.archive.org/ait/packages/+simple/" \
+    && bash -x -c "source /tmp/venv/bin/activate \
+            && sync.py >>/tmp/trough-sync-local.out 2>&1 &" \
+    && bash -x -c "source /tmp/venv/bin/activate \
+            && sleep 5 \
+            && python -c \\"import doublethink ; from trough.settings import settings ; rr = doublethink.Rethinker(settings[\'RETHINKDB_HOSTS\']) ; rr.db(\'trough_configuration\').wait().run()\\"" \
+    && bash -x -c "source /tmp/venv/bin/activate \
+            && sync.py --server >>/tmp/trough-sync-server.out 2>&1 &" \
+    && bash -x -c "source /tmp/venv/bin/activate \
+            && uwsgi --daemonize2 --venv=/tmp/venv --http :6444 --master --processes=2 --harakiri=3200 --socket-timeout=3200 --max-requests=50000 --vacuum --die-on-term --wsgi-file /tmp/venv/bin/reader.py >>/tmp/trough-read.out 2>&1 \
+            && uwsgi --daemonize2 --venv=/tmp/venv --http :6222 --master --processes=2 --harakiri=240 --max-requests=50000 --vacuum --die-on-term --wsgi-file /tmp/venv/bin/writer.py >>/tmp/trough-write.out 2>&1 \
+            && uwsgi --daemonize2 --venv=/tmp/venv --http :6112 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --mount /=trough.wsgi.segment_manager:local >>/tmp/trough-segment-manager-local.out 2>&1 \
+            && uwsgi --daemonize2 --venv=/tmp/venv --http :6111 --master --processes=2 --harakiri=20 --max-requests=50000 --vacuum --die-on-term --mount /=trough.wsgi.segment_manager:server >>/tmp/trough-segment-manager-server.out 2>&1 \
+            && pip install pytest \
+            && cd /tmp/trough \
+            && py.test -v tests"'
