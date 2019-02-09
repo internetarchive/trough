91a42b99f7a72e3c8328d3208dfbdccafb1e6b52
James Kafader <jkafader@archive.org>
2019-02-09 18:53:34 +0000

    trough client now supports multiple connections and query fanning

--- trough/cli/__init__.py
+++ trough/cli/__init__.py
@@ -8,6 +8,8 @@ import readline
 from prettytable import PrettyTable
 import datetime
 import pydoc
+import re
+import concurrent.futures
 
 HISTORY_FILE = os.path.expanduser('~/.trough_history')
 
@@ -33,54 +35,21 @@ class TroughRepl(cmd.Cmd):
     logger = logging.getLogger('trough.client.TroughRepl')
 
     def __init__(
-            self, trough_client, segment_id, writable=False,
+            self, trough_client, segments, writable=False,
             schema_id='default'):
         super().__init__()
         self.cli = trough_client
-        self.segment_id = segment_id
+        self.segments = segments
         self.writable = writable
         self.schema_id = schema_id
         self.pretty_print = True
-
-        self.prompt = 'trough:%s(%s)> ' % (
-                segment_id, 'rw' if writable else 'ro')
-
-    def do_show(self, argument):
-        '''SHOW command, like MySQL. Available subcommands:
-        - SHOW TABLES
-        - SHOW CREATE TABLE
-        - SHOW SCHEMA schema-name
-        - SHOW SCHEMAS
-        - SHOW SEGMENTS [MATCHING 'regexp']'''
-        argument = argument.replace(";", "").lower()
-        if argument[:6] == 'tables':
-            self.do_select("name from sqlite_master where type = 'table';")
-        elif argument[:12] == 'create table':
-            self.do_select(
-                    "sql from sqlite_master where type = 'table' "
-                    "and name = '%s';" % argument[12:].replace(';', '').strip())
-        elif argument[:7] == 'schemas':
-            result = self.cli.schemas()
-            self.display(result)
-        elif argument[:7] == 'schema ':
-            name = argument[7:].strip()
-            result = self.cli.schema(name)
-            self.display(result)
-        elif argument[:8] == 'segments':
-            regex = None
-            if "matching" in argument:
-                regex = argument.split("matching")[-1].strip().strip('"').strip("'")
-            try:
-                start = datetime.datetime.now()
-                result = self.cli.readable_segments(regex=regex)
-                end = datetime.datetime.now()
-                n_rows = self.display(result)
-                print("%s results in %s" % (n_rows, end - start))
-            except Exception as e:
-                self.logger.error(e, exc_info=True)
-        else:
-            self.do_help('show')
-
+        self.show_segment_in_result = False
+        self.workers = 20
+        logging.warn('populating segment cache...')
+        self._segment_cache = set(str(item) for item in self.cli.rr.table('services')['segment'].run())
+        logging.warn('done. %s segments' % len(self._segment_cache))
+        self.update_prompt()
+                        
     import sys
 
     def table(self, dictlist, outfile=sys.stdout):
@@ -125,6 +94,64 @@ class TroughRepl(cmd.Cmd):
             pydoc.pager(result)
             return len(result)
 
+    def update_prompt(self):
+        self.prompt = 'trough:%s(%s)> ' % (
+        self.segments[0] if len(self.segments) == 1 else '[%s segments]' % len(self.segments), 'rw' if self.writable else 'ro')
+
+        
+    def do_show(self, argument):
+        '''SHOW command, like MySQL. Available subcommands:
+        - SHOW TABLES
+        - SHOW CREATE TABLE
+        - SHOW CONNECTIONS
+        - SHOW SCHEMA schema-name
+        - SHOW SCHEMAS
+        - SHOW SEGMENTS [MATCHING 'regexp']'''
+        argument = argument.replace(";", "").lower()
+        if argument[:6] == 'tables':
+            self.do_select("name from sqlite_master where type = 'table';")
+        elif argument[:12] == 'create table':
+            self.do_select(
+                    "sql from sqlite_master where type = 'table' "
+                    "and name = '%s';" % argument[12:].replace(';', '').strip())
+        elif argument[:7] == 'schemas':
+            result = self.cli.schemas()
+            self.display(result)
+        elif argument[:11] == 'connections':
+            self.display([{'connection': segment } for segment in self.segments])
+        elif argument[:7] == 'schema ':
+            name = argument[7:].strip()
+            result = self.cli.schema(name)
+            self.display(result)
+        elif argument[:8] == 'segments':
+            regex = None
+            if "matching" in argument:
+                regex = argument.split("matching")[-1].strip().strip('"').strip("'")
+            try:
+                start = datetime.datetime.now()
+                result = self.cli.readable_segments(regex=regex)
+                end = datetime.datetime.now()
+                n_rows = self.display(result)
+                print("%s results in %s" % (n_rows, end - start))
+            except Exception as e:
+                self.logger.error(e, exc_info=True)
+        else:
+            self.do_help('show')
+
+    def do_connect(self, argument):
+        '''Connect to one or more trough "segments" (sqlite databases)'''
+        # for each connection string
+        splitter = re.compile(" +")
+        segment_patterns = splitter.split(argument)
+        # look for a regex match
+        matcher = re.compile("|".join(segment_patterns))
+        # create a connection for each segment returned
+        self.segments = [segment for segment in self._segment_cache if matcher.match(segment)]
+        # if we only have one connection, don't bother printing the segment
+        self.show_segment_in_result = len(self.segments) > 1
+        # update the prompt!
+        self.update_prompt()
+    
     def do_pretty(self, ignore):
         '''Toggle pretty-printed results'''
         self.pretty_print = not self.pretty_print
@@ -138,14 +165,29 @@ class TroughRepl(cmd.Cmd):
         Example: Send query "select * from host_statistics;" to server
         trough> query select * from host_statistics;
         '''
-        try:
-            start = datetime.datetime.now()
-            result = self.cli.read(self.segment_id, 'select ' + line)
-            end = datetime.datetime.now()
-            n_rows = self.display(result)
-            print("%s results in %s" % (n_rows, end - start))
-        except Exception as e:
-            self.logger.error(e, exc_info=True)
+        query = 'select ' + line
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
+            try:
+                start = datetime.datetime.now()
+                future_to_args = {executor.submit(self.cli.read, segment, query): (segment, query) for segment in self.segments }
+                n_rows = 0
+                for future in concurrent.futures.as_completed(future_to_args):
+                    segment, query = future_to_args[future]
+                    try:
+                        result = future.result()
+                        if self.show_segment_in_result:
+                            #import pdb; pdb.set_trace()
+                            result = list(result)
+                            for row in result:
+                                row.update({'__segment': segment})
+                    except Exception as e:
+                        logging.error('Error executing "%s" against %s: %s' % (query, segment, e))
+                    else:
+                        n_rows += self.display(result)
+                end = datetime.datetime.now()
+                print("%s results in %s" % (n_rows, end - start))
+            except Exception as e:
+                self.logger.error(e, exc_info=True)
 
     def emptyline(self):
         pass
@@ -199,7 +241,7 @@ def trough_client(argv=None):
                 '(%(filename)s:%(lineno)d) %(message)s'))
 
     cli = trough.client.TroughClient(args.rethinkdb_trough_db_url)
-    shell = TroughRepl(cli, args.segment, args.writable, args.schema)
+    shell = TroughRepl(cli, [args.segment], args.writable, args.schema)
 
     if os.path.exists(HISTORY_FILE):
         readline.read_history_file(HISTORY_FILE)
