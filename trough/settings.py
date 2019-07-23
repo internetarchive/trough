234d23113c4789719b1a18b33132493e70871840
James Kafader <jkafader@archive.org>
2019-07-22 17:13:01 -0700

    this setting should be present by default

--- trough/settings.py
+++ trough/settings.py
@@ -65,6 +65,7 @@ settings = {
     'LOG_LEVEL': 'INFO',
     'RUN_AS_COLD_STORAGE_NODE': False,
     'COLD_STORAGE_PATH': "/var/tmp/trough",
+    'COLD_STORE_SEGMENT': False,
     'ALLOWED_WRITE_VERBS': ['INSERT'], # allow inserts only by default. Enforces consistency. The user can optionally allow updates, create etc.
 }
 
