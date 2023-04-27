6bfb1121f85db91f019d43233d2edead218b7eb4
Barbara Miller <barbara@archive.org>
2023-04-27 00:15:47 +0000

    add pyarrow, rm snakebite & hdfs3; update aiodns

--- setup.py
+++ setup.py
@@ -21,18 +21,17 @@ setup(
     ],
     install_requires=[
         'protobuf>=3.7.1,<4',
+        'pyarrow==11.0.0,
         'PyYAML>=5.1',
         'requests>=2.21.0',
         'six>=1.10.0',
-        'snakebite-py3>=3.0',
         'ujson-ia>=2.1.1',
         'sqlparse>=0.2.2',
         'uWSGI>=2.0.15',
         'doublethink>=0.2.0',
         'uhashring>=0.7,<1.0',
         'flask>=1.0.2,<2',
-        'hdfs3>=0.2.0',
-        'aiodns>=1.2.0',
+        'aiodns==3.0.0',
         'aiohttp==3.8.4', # 3.8.4 requires python 3.6+
         'async-timeout==4.0.2', # 4.0.2 requires python 3.6+
     ],
